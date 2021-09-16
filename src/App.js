84d57b8d6bb4837a3f307fafa86257f5e297a2d7
MET-DEV <mustafatpe23@gmail.com>
2021-09-16 19:29:42 +0300

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, Card, CardContent, Form, Input} from 'semantic-ui-react'
+import * as Yup from 'yup'
+import { useFormik } from 'formik';
+import axios from 'axios';
 
 function App() {
+  const message="Bu alan boş bırakılamaz"
+  const schema=Yup.object().shape({
+    firstname:Yup.string().required(message),
+    lastname:Yup.string().required(message),
+    email:Yup.string().required(message),
+    password:Yup.string().required(message)
+  })
+  const formik=useFormik({
+    initialValues:{
+      firstname:"",
+      lastname:"",
+      email:"",
+      password:""
+    },validationSchema:schema,
+    onSubmit:(values)=>{
+      add(values)
+    }
+  })
+  function add(values) {
+    axios.post("http://localhost:8080/user",values).then((res)=>console.log(res))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form onSubmit={formik.handleSubmit}>
+        <Card centered style={{marginTop:"100px"}}>
+          <Card.Content>Ad</Card.Content>
+          <Form.Field><CardContent><Input value={formik.values.firstname} name="firstname" onChange={formik.handleChange} placeholder="isim"></Input></CardContent></Form.Field>
+          <Card.Content>Soyad</Card.Content>
+          <Form.Field><CardContent><Input value={formik.values.lastname} name="lastname" onChange={formik.handleChange} placeholder="soyad"></Input></CardContent></Form.Field>
+          <Card.Content>Email</Card.Content>
+          <Form.Field><CardContent><Input value={formik.values.email} name="email" onChange={formik.handleChange} placeholder="email"></Input></CardContent></Form.Field>
+          <Card.Content>Parola</Card.Content>
+          <Form.Field><CardContent><Input value={formik.values.password} name="password" onChange={formik.handleChange} placeholder="parola"></Input></CardContent></Form.Field>
+          <Form.Field><CardContent><Button type="submit" color="purple" style={{marginBottom:"70px"}}>Ekle</Button></CardContent></Form.Field>
+        </Card>
+      </Form>
     </div>
   );
 }
